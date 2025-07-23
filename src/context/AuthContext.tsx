23c0da344abcdd4456c6ebeccb5bae48ef2d41de
sikchang <changsik323@gmail.com>
2025-07-23 16:16:49 +0900

    [feat] 로그인 상태 전역 관리 #11

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,45 +1,8 @@
+import { createContext } from 'react'
 import type { AuthContextType } from '@/types/auth'
-import React, { createContext, useEffect, useState } from 'react'
 
 export const AuthContext = createContext<AuthContextType>({
   login: () => {},
   logout: () => {},
-  isAuthenticated: false,
-  token: null
+  isAuthenticated: false
 })
-
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isAuth, setIsAuth] = useState(false)
-  const [token, setToken] = useState<string | null>(null)
-
-  const login = (newToken: string) => {
-    setIsAuth(true)
-    setToken(newToken)
-    localStorage.setItem('authToken', newToken)
-
-    console.log('로그인 성공:', newToken)
-  }
-
-  const logout = () => {
-    setIsAuth(false)
-    setToken(null)
-    localStorage.removeItem('authToken')
-
-    console.log('로그아웃 성공')
-  }
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem('authToken')
-    if (storedToken) {
-      setIsAuth(true)
-      setToken(storedToken)
-    }
-  }, [])
-
-  return (
-    <AuthContext.Provider
-      value={{ isAuthenticated: isAuth, login, logout, token }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
