a06961a68e8325cbc25f75a4e552d2f598cd9430
sikchang <changsik323@gmail.com>
2025-07-24 14:38:49 +0900

    [Feat] 로그인 상태에 따라 헤더 버튼 동적 표시

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,5 +4,6 @@ import { createContext } from 'react'
 export const AuthContext = createContext<AuthContextType>({
   login: () => {},
   logout: () => {},
-  isAuthenticated: false
+  isAuthenticated: false,
+  authData: null
 })
--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,32 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import { AuthContext } from './AuthContext'
+import { useAuthLogin } from '@/hooks/auth/useAuthLogin'
+import type { AuthData } from '@/types/auth'
+import { mapUserDataToAuthData } from '@/utils/authMappers'
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuth, setIsAuth] = useState(false)
+  const [authData, setAuthData] = useState<AuthData | null>(null)
+  const { getSession } = useAuthLogin()
 
-  const login = (newToken: string) => {
+  const login = (userData: AuthData) => {
     setIsAuth(true)
-    localStorage.setItem('authToken', newToken)
-
-    console.log('로그인 성공:', newToken)
+    setAuthData(userData)
+    console.log('로그인 성공:', userData)
   }
 
   const logout = () => {
     setIsAuth(false)
-    localStorage.removeItem('authToken')
-
+    setAuthData(null)
     console.log('로그아웃 성공')
+
+    localStorage.clear()
   }
 
   useEffect(() => {
-    const storedToken = localStorage.getItem('authToken')
-    if (storedToken) {
-      setIsAuth(true)
+    const fetchSession = async () => {
+      const sessionData = await getSession()
+      if (sessionData !== undefined && sessionData !== null) {
+        setIsAuth(true)
+        setAuthData(mapUserDataToAuthData(sessionData))
+      } else {
+        setIsAuth(false)
+        setAuthData(null)
+      }
     }
+
+    fetchSession()
   }, [])
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated: isAuth, login, logout }}>
+    <AuthContext.Provider
+      value={{ isAuthenticated: isAuth, authData, login, logout }}>
       {children}
     </AuthContext.Provider>
   )
