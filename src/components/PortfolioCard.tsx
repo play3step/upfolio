833d393c060814cf403b57a3e3e41397f1956d2b
mintsky0172 <mintsky0172@naver.com>
2025-07-24 11:33:35 +0900

    [feat] 북마크 아이콘 조건부 렌더링, global.css 오타 수정

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -1,7 +1,10 @@
 import S from './PortfolioCard.module.css'
-import bookmarkIcon from '../assets/icon/bookmark.svg'
+import bookmarkIcon from '../assets/icon/bookmark-empty.svg'
+import bookmarkFilledIcon from '../assets/icon/bookmark-fill.svg'
 import grayHeart from '../assets/icon/grayHeart.svg'
 import eye from '../assets/icon/eye.svg'
+import { useState } from 'react'
+import supabase from '@/lib/supabaseClient'
 
 export interface Props {
   id: string
@@ -15,6 +18,8 @@ export interface Props {
 }
 
 export function PortfolioCard({
+  id,
+  userid,
   title,
   content,
   likecount,
@@ -22,35 +27,54 @@ export function PortfolioCard({
   interest,
   career
 }: Props) {
+  const [bookmarked, setBookmarked] = useState(false);
+
+  const toggleBookmark = async () => {
+    setBookmarked(!bookmarked);
+
+    const user = await supabase.auth.getUser();
+    const userId = user.data.user?.id;
+
+    if (!userId) return;
+
+    if (!bookmarked) {
+      await supabase.from('BookMark').insert({
+        userid: userId,
+        portfolioid: id
+      });
+    } else {
+      await supabase
+        .from('BookMark')
+        .delete()
+        .eq('userid', userId)
+        .eq('portfolioid', id);
+    }
+  };
+
   return (
     <div className={S.container}>
       <div className={S.interest}>{interest}</div>
-      <button className={S.bookmark}>
+      <button onClick={toggleBookmark} className={S.bookmark}>
         <img
-          src={bookmarkIcon}
-          alt="Bookmark Icon"
+          src={bookmarked ? bookmarkFilledIcon : bookmarkIcon}
+          alt="bookmark icon"
         />
       </button>
 
       <span className={S.career}> {career} </span>
       <h3 className={S.title}>{title}</h3>
-      <span className={S.content}> {content}</span>
+      <span className={S.content}>{content}</span>
+
       <div className={S.meta}>
         <span className={S.like}>
-          <img
-            src={grayHeart}
-            alt="Like Icon"
-          />
+          <img src={grayHeart} alt="Like Icon" />
           {likecount}
         </span>
         <span className={S.view}>
-          <img
-            src={eye}
-            alt="View Icon"
-          />
+          <img src={eye} alt="View Icon" />
           {viewcount}
         </span>
       </div>
     </div>
-  )
+  );
 }
