5b1a739d060272745f72e9f404ea3ca4e7106c83
echo <play3step@naver.com>
2025-07-24 15:28:56 +0900

    stlye : 채팅 목록 ui 축가

--- src/components/domain/dm/DmChatContainer.tsx
+++ src/components/domain/dm/DmChatContainer.tsx
@@ -1,12 +1,17 @@
 import S from './DmChatContainer.module.css'
 import dmSendIcon from '@/assets/icon/dm.svg'
+import DmChatListItem from './DmChatListItem'
 
 export default function DmChatContainer() {
   return (
     <div className={S['dm-chat-container']}>
-      <div className={S['dm-chat-container-left']}>DmChatContainer</div>
+      <div className={S['dm-chat-container-left']}>
+        <DmChatListItem />
+        <DmChatListItem />
+        <DmChatListItem />
+      </div>
       <div className={S['dm-chat-container-right']}>
-        <div className={S['dm-chat-conversation']}>DmChatContainer</div>
+        <div className={S['dm-chat-conversation']}></div>
         <div className={S['dm-chat-input']}>
           <textarea
             placeholder="채팅을 입력하세요."
