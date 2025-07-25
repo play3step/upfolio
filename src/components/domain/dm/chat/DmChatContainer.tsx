f22f1f71dbfc3432dcd0e9b4ccf9ae4fb13baf56
echo <play3step@naver.com>
2025-07-25 11:37:50 +0900

    style : 채팅 내용 ui 추가

--- src/components/domain/dm/chat/DmChatContainer.tsx
+++ src/components/domain/dm/chat/DmChatContainer.tsx
@@ -1,6 +1,7 @@
 import S from './DmChatContainer.module.css'
 import dmSendIcon from '@/assets/icon/dm.svg'
 import DmChatListItem from './DmChatListItem'
+import DmChatMessage from './DmChatMessage'
 
 interface Props {
   isOpen: boolean
@@ -17,7 +18,16 @@ export default function DmChatContainer({ isOpen }: Props) {
         <DmChatListItem />
       </div>
       <div className={S['dm-chat-container-right']}>
-        <div className={S['dm-chat-conversation']}></div>
+        <div className={S['dm-chat-conversation']}>
+          <DmChatMessage
+            message="에헤이 귀찮아"
+            isMine={true}
+          />
+          <DmChatMessage
+            message="그렇게 하는거 아닌데 뭐 하는거야"
+            isMine={false}
+          />
+        </div>
         <div className={S['dm-chat-input']}>
           <textarea
             placeholder="채팅을 입력하세요."
