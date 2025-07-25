d644072d021a3ab4bce1e680f6ab6070290d138a
mintsky0172 <mintsky0172@naver.com>
2025-07-25 12:19:06 +0900

    [design] 메인페이지 검색창  UI 수정

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,8 +2,29 @@ import Input from './common/Input'
 import S from './SearchBar.module.css'
 import search from '../assets/icon/search.svg'
 import RadioGroup from './common/RadioGroup'
+import { useState } from 'react'
+
+const INTEREST_SELECT = [
+    { label: '전체', value: 'all' },
+    { label: '프론트엔드', value: 'FE'},
+    { label: '백엔드', value: 'BE' },
+    { label: '풀스택', value: 'FullStack' },
+    { label: '모바일', value: 'Mobile' },
+    { label: '임베디드', value: 'Embedded' },
+    { label: 'UI/UX 디자인', value: 'UIUX' },
+    { label: '그래픽 디자인', value: 'Graphic'},
+    { label: '모션 디자인', value: 'Motion'},
+    { label: '일러스트', value: 'Illustration'},
+  ]
 
 export const SearchBar = () => {
+
+  const [interest, setInterest] = useState<string>('all')
+
+  const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInterest(e.target.value); 
+  };
+
   return (
     <div className={S.container}>
       <div className={S.filters}>
@@ -32,17 +53,11 @@ export const SearchBar = () => {
 
         <RadioGroup
           label={'지원 분야'}
-          options={[
-            { label: '전체', value: 'all' },
-            { label: '프론트엔드 개발자', value: 'frontend' },
-            { label: '백엔드 개발자', value: 'backend' },
-            { label: '모바일 개발자', value: 'mobile' },
-            { label: '웹 디자이너', value: 'design' }
-          ]}
+          options={INTEREST_SELECT}
           name={'searchField'}
           className={S.radioGroup}
-          checked={'all'}
-          onChange={() => {}}
+          checked={interest}
+         onChange={handleRadioChange}
         />
       </div>
     </div>
