1f11b5e7aaf5274c04cf45f2125efd6010e2f9a1
sikchang <changsik323@gmail.com>
2025-07-28 15:50:24 +0900

    [feat] 내 프로필 페이지 (진행중)

--- src/pages/MyPage/Profile.tsx
+++ src/pages/MyPage/Profile.tsx
@@ -2,6 +2,7 @@ import Button from '@/components/common/Button'
 import CheckboxSelect from '@/components/common/CheckboxSelect'
 import ImageUploader from '@/components/common/ImageUploader'
 import { useState } from 'react'
+import S from './Profile.module.css'
 
 export default function Profile() {
   const [selectedTechStack, setSelectedTechStack] = useState<string[]>([])
@@ -11,17 +12,25 @@ export default function Profile() {
   }
 
   return (
-    <div>
-      <h1>내프로필</h1>
-      <div>
-        <ImageUploader id="profile-image" />
-        <p>
-          안녕하세요,User님. <br />
-          오늘도 파이팅하세요!
-        </p>
-        <Button>프로필 수정</Button>
-        <hr />
-        <div>
+    <div className={S.profile}>
+      <div className={S.profile__content}>
+        <div className={S.profile__title}>
+          <h1>내 프로필</h1>
+          <div className={S.profile__btnWrap}>
+            <Button>프로필 수정</Button>
+          </div>
+        </div>
+        <div className={S.profile__imageSection}>
+          <ImageUploader id="profile-image" />
+          <p className={S.profile__greeting}>
+            안녕하세요, <strong>User</strong>님 <br />
+            오늘도 파이팅하세요!
+          </p>
+        </div>
+
+        <hr className={S.profile__divider} />
+
+        <div className={S.profile__checkbox}>
           <p>{selectedTechStack.join(' | ') || '선택된 항목이 없습니다.'}</p>
           <CheckboxSelect onChange={handleCheckboxChange} />
         </div>
