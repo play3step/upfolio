cd26c23a53360e6de616b9eaf2caeb092dece0fd
echo <play3step@naver.com>
2025-07-31 15:09:35 +0900

    [Feat] 알림 선택 추가

--- src/components/domain/alarm/alarmWrapper.tsx
+++ src/components/domain/alarm/alarmWrapper.tsx
@@ -1,16 +1,30 @@
 import S from './alarmWrapper.module.css'
 import AlarmCard from './alarmCard'
+import { useState } from 'react'
 
 export default function AlarmWrapper() {
+  const [selected, setSelected] = useState<'comment' | 'like' | 'dm'>('comment')
   return (
     <div className={S['alarm-wrapper']}>
       <div className={S['alarm-header']}>
         <h1>알림</h1>
       </div>
       <div className={S['alarm-select']}>
-        <p>댓글</p>
-        <p>좋아요</p>
-        <p>DM</p>
+        <p
+          className={selected === 'comment' ? S['alarm-select-active'] : ''}
+          onClick={() => setSelected('comment')}>
+          댓글
+        </p>
+        <p
+          className={selected === 'like' ? S['alarm-select-active'] : ''}
+          onClick={() => setSelected('like')}>
+          좋아요
+        </p>
+        <p
+          className={selected === 'dm' ? S['alarm-select-active'] : ''}
+          onClick={() => setSelected('dm')}>
+          DM
+        </p>
       </div>
       <div className={S['alarm-content']}>
         <AlarmCard />
