0b4bd8f771b29d2104ea1a1a194e3cf5a04da5d4
echo <play3step@naver.com>
2025-08-04 21:21:05 +0900

    [Fix] 회원가입후 메인 페이지 이동 오류 수정

--- src/hooks/auth/useSignup.ts
+++ src/hooks/auth/useSignup.ts
@@ -1,12 +1,12 @@
 import { signupUser } from '@/apis/user/signup.controller'
-import { useAuthLogin } from '@/hooks/auth/useAuthLogin'
+
 import { AuthContext } from '@/context/auth/AuthContext'
 import { formatPhoneNumber } from '@/utils/format'
 import { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import { alertSuccess } from '@/utils/alertUtils'
 export const useSignup = () => {
-  const { authData } = useContext(AuthContext)
+  const { authData, login } = useContext(AuthContext)
 
   const [nickname, setNickname] = useState(authData?.nickname ?? '')
   const [birthDate, setBirthDate] = useState({
@@ -15,8 +15,9 @@ export const useSignup = () => {
     day: ''
   })
   const [phone, setPhone] = useState('')
-  const { getSession } = useAuthLogin()
+
   const navigate = useNavigate()
+
   const handleBirthChange = (
     field: 'year' | 'month' | 'day',
     value: string
@@ -52,14 +53,35 @@ export const useSignup = () => {
       birthDate: `${birthDate.year}.${birthDate.month}.${birthDate.day}`,
       id: authData?.id ?? ''
     }
-    await signupUser(
+    const userData = await signupUser(
       signupData.nickname,
       signupData.phone,
       signupData.birthDate,
       signupData.id
     )
-    await getSession()
-    alert('회원가입이 완료되었습니다.')
+
+    const fetchData = {
+      id: authData?.id ?? '',
+      nickname: nickname ?? '',
+      phone: phone ?? '',
+      birthDate: `${birthDate.year}.${birthDate.month}.${birthDate.day}`,
+      email: authData?.email ?? '',
+      profileimage: authData?.profileimage ?? '',
+      createdat: authData?.createdat ?? '',
+      career: authData?.career ?? '',
+      interest: authData?.interest ?? '',
+      techstack: authData?.techstack ?? []
+    }
+
+    if (userData) {
+      alertSuccess({
+        title: '회원가입 완료',
+        text: '회원가입이 완료되었습니다.',
+        icon: 'success'
+      })
+      login(fetchData)
+    }
+
     navigate('/', { replace: true })
   }
 
